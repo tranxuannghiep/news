599221d2052a5b6e4557c76c6741ef139b716f86
NghiepRadeon <nghiepradeon@gmail.com>
2022-05-20 07:36:07 +0700

    dsd

--- controllers/newsControllers.js
+++ controllers/newsControllers.js
@@ -1,11 +1,21 @@
 const catchAsync = require("../middlewares/async");
 const News = require("../models/News");
 const ApiError = require("../utils/ApiError");
-
+let Parser = require("rss-parser");
+let parser = new Parser();
 exports.getNews = catchAsync(async (req, res) => {
-  const news = await News.find();
-  if (!news.length) {
-    throw new ApiError(400, "No news");
-  }
-  res.json({ success: true, data: news });
+  (async () => {
+    let feed = await parser.parseURL("https://vnexpress.net/rss/thoi-su.rss");
+
+    console.log(feed.title);
+
+    feed.items.forEach((item) => {
+      console.log(item.title);
+    });
+    res.json({ success: true, data: feed });
+  })();
+  // const news = await News.find();
+  // if (!news.length) {
+  //   throw new ApiError(400, "No news");
+  // }
 });
